37fd3d3a7b66ed857ead8c4be45743f79fb94a62
Aimee Wirick <73860287+aimeewirick@users.noreply.github.com>
2024-08-29 14:35:33 -0700

    Added character speed and size perspective changes based on close and far away from screen

--- main.py
+++ main.py
@@ -18,13 +18,7 @@ emily_sheet = pygame.image.load('Emily.png').convert_alpha()
 flying_sheet = pygame.image.load('Emily_fairy.png').convert_alpha()
 
 BLACK = (0, 0, 0)
-global PLAYER_X
-global PLAYER_Y
-l_images = []
-r_images = []
-u_images = []
-d_images = []
-flying_images = []
+
 clock = pygame.time.Clock()
 
 
@@ -36,7 +30,7 @@ def get_image(sheet, line, frame, width, height, scale, color):
     return image
 
 
-for x in range(4): #settinp up facing sprite sheets
+"""for x in range(4): #settinp up facing sprite sheets
     l_images.append(get_image(emily_sheet, 3, x, 16, 32, 3, BLACK))
     r_images.append(get_image(emily_sheet, 1, x, 16, 32, 3, BLACK))
     u_images.append(get_image(emily_sheet, 2, x, 16, 32, 3, BLACK))
@@ -44,7 +38,7 @@ for x in range(4): #settinp up facing sprite sheets
 
 
 for x in range(4):
-    flying_images.append(get_image(flying_sheet, 0, x, 28, 35, 3, BLACK))
+    flying_images.append(get_image(flying_sheet, 0, x, 28, 35, 3, BLACK))"""
 
 
 class Character:
@@ -71,8 +65,22 @@ def main():
     # player X = right left
     PLAYER_X = 600
 
+    # player scale aka Z coordinate
+    PLAYER_Z = 3
+
+    # x coordinate walk speed
+    X_speed = 8
+
+    # y coordinate walk speed
+    Y_speed = 2
+    # initialize character sprite sheet
+    char_images = []
+
     # set run game to True
     run = True
+
+    #set character sprite sheet as emily_sheet (will be a selectable variable later
+    character_sheet = emily_sheet
 
     # track sprite image iteration
     counter = 0
@@ -96,18 +104,16 @@ def main():
 
                 """Left direction from arrow or 'a' key"""
                 if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                    PLAYER_X -= 6
+                    PLAYER_X -= X_speed
                     facing = 3
-                    image = l_images[counter]
                     if counter == 3:
                         counter = 0
                     else:
                         counter += 1
                 """Right direction from arrow or 'd' key"""
                 if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                    PLAYER_X += 6
+                    PLAYER_X += X_speed
                     facing = 1
-                    image = r_images[counter]
                     if counter == 3:
                         counter = 0
                     else:
@@ -115,9 +121,14 @@ def main():
                 """Up direction from arrow or 'w' key"""
                 if event.key == pygame.K_UP or event.key == pygame.K_w:
                     if HEIGHT - PLAYER_Y < 900:
-                        PLAYER_Y -= 6
+                        PLAYER_Y -= Y_speed
+                    if X_speed > 5:
+                        X_speed -= 2
+                    if Y_speed > 1:
+                        Y_speed -= 1
+                    if PLAYER_Z >= 2:
+                        PLAYER_Z -= .5
                     facing = 2
-                    image = u_images[counter]
                     if counter == 3:
                         counter = 0
                     else:
@@ -125,29 +136,36 @@ def main():
                 """Down direction from arrow or 's' key"""
                 if event.key == pygame.K_DOWN or event.key == pygame.K_s:
                     if HEIGHT - PLAYER_Y > 100:
-                        PLAYER_Y += 6
+                        PLAYER_Y += Y_speed
+                    X_speed += 2
+                    if Y_speed > 1:
+                        Y_speed -= 1
+
+                    PLAYER_Z += .5
                     facing = 0
-                    image = d_images[counter]
                     if counter == 3:
                         counter = 0
                     else:
                         counter += 1
                 """Flying direction from space bar and up arrow"""
                 if event.key == pygame.K_SPACE and pygame.K_UP:  # not working properly yet
+                    character_sheet = flying_sheet
                     if HEIGHT - PLAYER_Y < 900:
                         PLAYER_Y -= 6
-                    image = flying_images[counter]
                     if counter == 3:
                         counter = 0
                     else:
                         counter += 1
                 """Draw player to screen using created variables"""
+                print(Y_speed, "y speed")
+                print(X_speed, "x speed")
+                image = get_image(character_sheet, facing, counter, 16, 32, PLAYER_Z, BLACK)
                 player = Character(image, PLAYER_X, PLAYER_Y)
                 draw(player)
                 pygame.display.update()
             else:
                 """Draw player using default variables when no keys are pushed"""
-                image = get_image(emily_sheet, facing, 0, 16, 32, 3, BLACK)
+                image = get_image(character_sheet, facing, 0, 16, 32, PLAYER_Z, BLACK)
                 player = Character(image, PLAYER_X, PLAYER_Y)
                 draw(player)
                 pygame.display.update()
